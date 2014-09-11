c407a59495fb6732177e8b41b1df01690656eb7b
KC <kccowan@yahoo.com>
2014-09-11 15:18:14 -0400

    Example of making non-generic class

---
+++ NonGeneric/NonGeneric/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caller = new Caller();
+
+            var result1 = caller.CallOp1(10, 5);
+            Console.WriteLine("Op1 result is {0}", result1);
+
+            var result2 = caller.CallOp2(10, 5);
+            Console.WriteLine("Op2 result is {0}", result2);
+
+            var result3 = caller.CallOp3(10, 5);
+            Console.WriteLine("Op3 result is {0}", result3);
+
+
+        }
+    }
+}
---
+++ NonGeneric/NonGeneric/StackOverflow.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace NonGeneric
+{
+    class Stackoverflow<T>
+    {
+        private Func<IConvertible, IFormatProvider, T> convertValue;
+        private Func<T, T, T> op;
+
+        public Stackoverflow(Func<IConvertible, IFormatProvider, T> ToGenericValue, Func<T, T, T> opGeneric)
+        {
+            this.convertValue = ToGenericValue;
+            this.op = opGeneric;
+        }
+
+        private T convert64(IConvertible val, IFormatProvider f)
+        {
+            return (T)(IConvertible)val.ToInt64(f);
+        }
+
+        /* This is what i want to write */
+        public Stackoverflow(Func<T, T, T> opGeneric) 
+        {
+            //this.convertValue = convert64;
+            this.convertValue = (val, f) => { return (T)(IConvertible)val.ToInt64(f); };
+            this.op = opGeneric;
+        }
+
+        public T Operation<K>(K a, K b) where K : IConvertible
+        {
+            T aAsT = this.convertValue(a, CultureInfo.InvariantCulture);
+            T bAsT = this.convertValue(b, CultureInfo.InvariantCulture);
+            return op(aAsT, bAsT);
+        }
+    }
+
+    class Caller
+    {
+        Stackoverflow<Int64> op1 = new Stackoverflow<Int64>(
+         (x, y) => { return x / y; }
+         );
+
+        Stackoverflow<Int64> op2 = new Stackoverflow<Int64>(
+            (x, y) => { return x + y; }
+            );
+
+        Stackoverflow<Int64> op3 = new Stackoverflow<Int64>(
+            (x, y) => { return (x - y) * x; }
+            );
+
+        public Int64 CallOp1(Int64 a, Int64 b)
+        {
+            return op1.Operation(a,b);
+
+        }
+        public Int64 CallOp2(Int64 a, Int64 b)
+        {
+            return op2.Operation(a, b);
+
+        }
+
+        public Int64 CallOp3(Int64 a, Int64 b)
+        {
+            return op3.Operation(a, b);
+
+        }
+    }
+}
+
