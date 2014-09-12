18faae36e925fa60829bf86e3c0eef9297347554
KC <kccowan@yahoo.com>
2014-09-12 14:57:14 -0400

    Hashtable
    Hashtable example

---
+++ HashtableExample/HashtableExample/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace HashtableExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Hashtable Info = new Hashtable();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            string b = textBox2.Text;
+            Info.Add(a, b);
+            label4.Text = a + " " + b;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder allEntries = new StringBuilder();
+            foreach (DictionaryEntry DE in Info)
+            {
+                allEntries.Append(DE.Key);
+                allEntries.Append(DE.Value);
+            }
+            label4.Text = allEntries.ToString();
+        }
+
+    }
+}
