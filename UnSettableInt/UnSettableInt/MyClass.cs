057aa8458a06343dcd1a0164e2beebb75a2d3ad3
KC <kccowan@yahoo.com>
2014-09-29 19:58:14 -0400

    Attempting to hide set accessor
    Note this doesn't do quite what was asked

---
+++ UnSettableInt/UnSettableInt/MyClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnSettableInt
+{
+    public class MyClass
+    {
+        public class UnSettable 
+        {
+            public UnSettable(int v)
+            {
+                Console.WriteLine("Unsettable constructor value {0}", v);
+                _value = v;
+            }
+
+            public static implicit operator UnSettable (int v)
+            {
+                Console.WriteLine("Unsettable implicit conversion to UnSettable {0}", v);
+                return new UnSettable(v);
+            }
+
+            public static implicit operator int(UnSettable v)
+            {
+                Console.WriteLine("Unsettable implicit conversion to int {0}", v);
+                return v._value;
+            }
+
+            public static UnSettable operator ++ (UnSettable v)
+            {
+                UnSettable result = new UnSettable(v._value + 1);
+                Console.WriteLine("Unsettable operator++ {0}", result._value);
+                return result;
+            }
+
+            public int _value = 0;
+        }
+
+        UnSettable _count = 10;
+
+        public UnSettable Count
+        {
+            get { Console.WriteLine("Count get accessor");  return _count; }
+            set { Console.WriteLine("Count set accessor");  _count = value; }
+        }
+    }
+}
---
+++ UnSettableInt/UnSettableInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnSettableInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tConstructing MyClass c");
+            MyClass c = new MyClass();
+            Console.WriteLine("\n\t\tTest1: Assign value 14");
+            c.Count = 14;
+            Console.WriteLine("\n\t\tTest2: operator++ to yield 15");
+            c.Count++;
+
+            Console.WriteLine("");
+        }
+    }
+}
