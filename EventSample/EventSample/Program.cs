b5c780d7c9d3b66ea5b54d0518f8f648e79ac14a
KC <kccowan@yahoo.com>
2014-10-22 13:42:49 -0400

    Sample of using event and delegates
    Cannot directly call the add routine (nor set for properties)

---
+++ EventSample/EventSample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSample
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var eg = new Example();
+            eg.MyProperty = 42;
+            eg.RegisterClickHandler();
+            eg.InvokeClickHandler("Hello world");
+        }
+    }
+
+    public class Example
+    {
+
+        public int MyProperty { get; set; }
+
+        delegate void ClickHandler(String arg, EventArgs e);
+        event ClickHandler click;
+
+        public void RegisterClickHandler()
+        {
+            click += new ClickHandler(MyHandler);
+        }
+
+        public void InvokeClickHandler(string arg)
+        {
+            click(arg, new EventArgs());
+        }
+
+        private void MyHandler(string arg, EventArgs e)
+        {
+            int prop = MyProperty;
+            Console.WriteLine("In MyHandler with arg: {0} and {1}", arg, prop);
+        }
+
+
+
+
+    }
+}
